f6b7bb39d12721a408903255dd976fd2c7d81959
svancise <slvancise@gmail.com>
2018-02-02 08:24:37 -0500

    made err change

--- src/components/Err.js
+++ src/components/Err.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Err = ({error}) => (
+const Err = ({error}) => {
+	return (
 	<div style={{'textAlign': 'center'}}>
-		<b>{error.graphQLErrors[0].message}</b>
+		<b>{error.graphQLErrors[0] ? error.graphQLErrors[0].message : 'An error occured, did you add an access token?'}</b>
 	</div>
-);
+)};
 
 
 export default Err;
